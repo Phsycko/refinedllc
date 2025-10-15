35ae1925c9045d1dc754b20fb05dff0b1a94b06a
Jose Manuel Camacho Venzor <josecamachojsmith99@gmail.com>
2025-10-14 21:33:31 -0400

    Convert logo to PNG format as requested

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
             <Link href="/" className="flex items-center space-x-3">
               <div className="h-16 w-auto">
                 <img 
-                  src="/logo.svg" 
+                  src="/logo.png" 
                   alt="Refined LLC" 
                   className="h-full w-auto"
                 />
