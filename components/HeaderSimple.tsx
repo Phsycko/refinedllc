4c6fe6c350132724e9717008bb008c4c4382aa63
Jose Manuel Camacho Venzor <josecamachojsmith99@gmail.com>
2025-10-18 18:48:56 -0400

    Restore original logo from Google Drive link

--- components/HeaderSimple.tsx
+++ components/HeaderSimple.tsx
@@ -35,7 +35,7 @@ export default function HeaderSimple() {
           <Link href="/" className="flex items-center space-x-3">
             <div className="h-32 w-auto">
               <img 
-                src="/logo.svg" 
+                src="/logo.png" 
                 alt="Refined LLC" 
                 className="h-full w-auto"
               />
