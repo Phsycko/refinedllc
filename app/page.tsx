cde7eea62a4d932ef8866b70f7b8239d7cea07f2
Jose Manuel Camacho Venzor <josecamachojsmith99@gmail.com>
2025-10-22 21:38:45 -0400

    Replace Hero with Header component on homepage to show carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Hero from '@/components/Hero'
+import Header from '@/components/Header'
 import ServicesGrid from '@/components/ServicesGrid'
 import FeaturedSection from '@/components/FeaturedSection'
 import ProcessSteps from '@/components/ProcessSteps'
@@ -8,25 +8,11 @@ import ProjectsGrid from '@/components/ProjectsGrid'
 import TestimonialList from '@/components/TestimonialList'
 import ValueStats from '@/components/ValueStats'
 import CTASection from '@/components/CTASection'
-import { useLanguage } from '@/contexts/LanguageContext'
 
 export default function HomePage() {
-  const { t } = useLanguage()
-
   return (
     <>
-      <Hero
-        title={t.hero.title}
-        subtitle={t.hero.subtitle}
-        primaryCTA={{
-          text: t.hero.viewProjects,
-          href: '/proyectos',
-        }}
-        secondaryCTA={{
-          text: t.hero.ourServices,
-          href: '/servicios',
-        }}
-      />
+      <Header />
       
       <ServicesGrid />
       
